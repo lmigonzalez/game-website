200714074a7a55630dc627938c522c80445952fb
lmigonzalez <lmigonzalezs@gmail.com>
2023-03-13 19:08:33 -0400

    countdown

--- src/sections/Home/CountDown.tsx
+++ src/sections/Home/CountDown.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Audiowide } from 'next/font/google';
-
+import moment from 'moment';
+import { motion, AnimatePresence } from 'framer-motion';
 const audiowide = Audiowide({
   weight: ['400'],
   subsets: ['latin'],
@@ -10,92 +11,145 @@ interface CountdownProps {
   duration: number;
 }
 
-const CountDown: React.FC<CountdownProps> = ({ duration }) => {
-  const [countdown, setCountdown] = useState<number>(duration);
-  const [previousCountdown, setPreviousCountdown] = useState<number>(
-    duration - 1
+const CountDown: React.FC<CountdownProps> = () => {
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [initialValue, setInitialValue] = useState(222545);
+
+  const time = moment({
+    hour: Math.floor(initialValue / 10000),
+    minute: Math.floor(initialValue / 100) % 100,
+    second: initialValue % 100,
+  });
+  const seconds = time.second();
+  const minutes = time.minute();
+  const hours = time.hour();
+
+  const [secondLast, setSecondLast] = useState(9);
+  const [secondFirst, setSecondFirst] = useState(5);
+
+  const [minutesLast, setMinutesLast] = useState(
+    parseInt(minutes.toString()[0])
   );
-  const [animate, setAnimate] = useState<boolean>(false);
+  const [minutesFirst, setMinutesFirst] = useState(
+    parseInt(minutes.toString()[1])
+  );
+
+  const [hoursLast, setHoursLast] = useState(parseInt(hours.toString()[0]));
+  const [hoursFirst, setHoursFirst] = useState(parseInt(hours.toString()[1]));
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setInitialValue(initialValue - 1);
+          secondsCountDown();
+          minutesCountDown();
+          hoursCountDown();
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [initialValue]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setPreviousCountdown(countdown);
-      setCountdown(countdown - 1);
-      setAnimate(true);
-      setTimeout(() => setAnimate(false), 1000);
+    if (timeLeft === 0) return;
+
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
     }, 1000);
 
-    return () => clearInterval(interval);
-  }, [countdown]);
+    return () => clearInterval(intervalId);
+  }, [timeLeft]);
 
-  const hours: number = Math.floor(countdown / 3600);
-  const minutes: number = Math.floor((countdown % 3600) / 60);
-  const seconds: number = countdown % 60;
+  function secondsCountDown() {
+    setSecondLast(secondLast === 0 ? 9 : secondLast - 1);
+
+    if (secondLast === 0) {
+      setSecondFirst(secondFirst === 0 ? 5 : secondFirst - 1);
+    }
+  }
+
+  function minutesCountDown() {
+    if (secondFirst === 0 && secondLast === 0) {
+      setMinutesLast(minutesLast === 0 ? 9 : minutesLast - 1);
+    }
+    if (minutesLast === 0 && secondFirst === 0 && secondLast === 0) {
+      setMinutesFirst(minutesFirst === 0 ? 5 : minutesFirst - 1);
+    }
+  }
+
+  function hoursCountDown() {
+    if (hoursFirst === 0 && hoursLast === 0) {
+      return;
+    }
+    if (
+      minutesFirst === 0 &&
+      minutesLast === 0 &&
+      secondFirst === 0 &&
+      secondLast === 0
+    ) {
+      setHoursLast(hoursLast === 0 ? 9 : hoursLast - 1);
+    }
+    if (
+      hoursLast === 0 &&
+      minutesFirst === 0 &&
+      minutesLast === 0 &&
+      secondFirst === 0 &&
+      secondLast === 0
+    ) {
+      setHoursFirst(hoursFirst > 0 ? hoursFirst - 1 : 0);
+    }
+  }
+
   return (
-    <div className="relative w-48 h-24">
-      <div className="absolute inset-0 flex flex-col justify-between">
-        <div className="text-center">
-          <span
-            className={`text-4xl font-bold ${
-              animate ? 'animate-countdown-new' : ''
-            }`}
-            data-countdown={hours}
-          >
-            {hours.toString().padStart(2, '0')}
-          </span>
-          <span className="text-lg font-medium text-gray-500">Hours</span>
-        </div>
-        <div className="text-center">
-          <span
-            className={`text-4xl font-bold ${
-              animate ? 'animate-countdown-new' : ''
-            }`}
-            data-countdown={minutes}
-          >
-            {minutes.toString().padStart(2, '0')}
-          </span>
-          <span className="text-lg font-medium text-gray-500">Minutes</span>
-        </div>
-        <div className="text-center relative">
-          <span
-            className={`text-4xl font-bold ${
-              animate ? 'animate-countdown-new' : ''
-            }`}
-            data-countdown={seconds}
-            style={{ top: animate ? '-100%' : 0 }}
-          >
-            {seconds.toString().padStart(2, '0')}
-          </span>
-          <span className="text-lg font-medium text-gray-500 absolute top-0 w-full">
-            Seconds
-          </span>
-          <span
-            className={`text-4xl font-bold ${
-              animate ? 'animate-countdown-old' : ''
-            }`}
-            data-countdown={previousCountdown % 60}
-            style={{ top: animate ? 0 : '100%' }}
-          >
-            {previousCountdown % 60}
-          </span>
+    <section className={`${audiowide.className}`}>
+      <div className=" bg-black py-8 px-10">
+        <div className="flex w-full items-center justify-center space-x-24 text-center">
+          <div className="">
+            <div className="space-x-4">
+              <span className="orange-gradient bg-clip-text text-8xl text-transparent">
+                {hoursFirst || 0}
+              </span>
+              <span className="orange-gradient bg-clip-text text-8xl text-transparent">
+                {hoursLast}
+              </span>
+            </div>
+            <p className="orange-gradient bg-clip-text  text-transparent">
+              HOURS
+            </p>
+          </div>
+
+          <div className="">
+            <div className="space-x-4">
+              <span className="orange-gradient bg-clip-text text-8xl text-transparent">
+                {minutesFirst}
+              </span>
+              <span className="orange-gradient bg-clip-text text-8xl text-transparent">
+                {minutesLast}
+              </span>
+            </div>
+            <p className="orange-gradient bg-clip-text  text-transparent">
+              MINUTES
+            </p>
+          </div>
+          <div className="">
+            <div className="space-x-4">
+              <span className="orange-gradient relative top-0 bg-clip-text text-8xl text-transparent">
+                {secondFirst}
+              </span>
+
+              <span className="orange-gradient bg-clip-text text-8xl text-transparent">
+                {secondLast}
+              </span>
+            </div>
+
+            <p className="orange-gradient bg-clip-text  text-transparent">
+              SECONDS
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
 export default CountDown;
 
 {
-  /* <div className="py-8 px-10 bg-black w-full flex justify-center">
-  <div className="text-transparent bg-clip-text orange-gradient text-8xl">
-    00:
-  </div>
-  <div className="text-transparent bg-clip-text orange-gradient text-8xl">
-    00:
-  </div>
-  <div className="text-transparent bg-clip-text orange-gradient text-8xl">
-    00
-  </div>
-</div>; */
 }
