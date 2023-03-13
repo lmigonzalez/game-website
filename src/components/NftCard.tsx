2750ce1701676a5bc30cb2ab53625a554a587c06
joszze <jossecarlos@gmail.com>
2023-03-13 12:34:09 -0400

    NftCard component init

---
+++ src/components/NftCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface INftCard{
+    id:string
+    name:string
+    packsCount: number
+    collectiblesCount: number
+    price:number
+    packsLeft: number
+}
+
+export function NftCard() {
+  return (
+      <div className="flex flex-col">
+          
+          
+    </div>
+  );
+}
+export default NftCard;
