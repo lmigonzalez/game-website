5f769ad4c0ee6285b5ccd33b571a37a916b8fc72
joszze <jossecarlos@gmail.com>
2023-03-14 04:09:53 -0400

    daisyui for modal

--- src/components/MintButton.tsx
+++ src/components/MintButton.tsx
@@ -1,7 +1,12 @@
 export default function MintButton() {
   return (
     <>
-      <button className=""> Mint Now</button>
+      <label
+        htmlFor="my-modal"
+        className="orange-gradient btn rounded-full text-center font-[roboto]"
+      >
+        Mint Now
+      </label>
     </>
   );
 }
