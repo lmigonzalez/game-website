7355036ee71a38c5fb811b200c36eb227589cba4
joszze <jossecarlos@gmail.com>
2023-03-14 01:13:15 -0400

    mint-btn

--- src/components/NftDetail.tsx
+++ src/components/NftDetail.tsx
@@ -79,7 +79,7 @@ export function NftDetail(props: INftDetail) {
             </span>
           </div>
         </div>
-        <MintButton nftId={props.id} />
+        <MintButton />
       </div>
     </div>
   );
