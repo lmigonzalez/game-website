d326f66b7b71c004d26aab0fc7bc1b814f4c2a9b
lmigonzalez <lmigonzalezs@gmail.com>
2023-03-16 00:08:57 -0400

    layout added

--- src/pages/dropv.tsx
+++ src/pages/dropv.tsx
@@ -1,32 +1,29 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import Navbar from "@/components/Navbar";
-import { testNft } from "./api/testdb";
-import Footer from "@/components/Footer";
-import NftCard from "@/components/NftCard";
-import classNames from "classnames";
-import { useState } from "react";
-import Info from "@/components/info";
-
+import { testNft } from './api/testdb';
+import NftCard from '@/components/NftCard';
+import classNames from 'classnames';
+import { useState } from 'react';
+import Info from '@/components/info';
+import Layout from '@/components/Layout';
 const cn = classNames;
 
 export default function dropv() {
   const [showInfo, setShowInfo] = useState<boolean>(true);
   return (
-    <>
-      <Navbar />
-      <main className="">
+    <Layout>
+      <main className="bg-white">
         <div className="tabs flex justify-center gap-4 ">
           <a
-            className={cn("tab tab-bordered w-12 text-2xl text-[roboto]", {
-              " text-color border-gradient-orange  tab-active": showInfo,
+            className={cn('tab-bordered tab w-12 text-2xl text-[roboto]', {
+              ' text-color border-gradient-orange  tab-active': showInfo,
             })}
             onClick={() => setShowInfo(true)}
           >
             Info
           </a>
           <a
-            className={cn("tab tab-bordered w-14 text-2xl text-[roboto]", {
-              "text-color border-gradient-orange tab-active": !showInfo,
+            className={cn('tab-bordered tab w-14 text-2xl text-[roboto]', {
+              'text-color border-gradient-orange tab-active': !showInfo,
             })}
             onClick={() => setShowInfo(false)}
           >
@@ -36,19 +33,18 @@ export default function dropv() {
 
         <div
           className={cn(
-            "m-10 grid grid-flow-col grid-rows-2 gap-10 whitespace-nowrap",
-            { "hidden ": showInfo }
+            'm-10 grid grid-flow-col grid-rows-2 gap-10 whitespace-nowrap',
+            { 'hidden ': showInfo }
           )}
         >
           {testNft.map((item, i) => (
             <NftCard {...item} key={i} />
           ))}
         </div>
-        <div className={cn({ "hidden ": !showInfo })}>
+        <div className={cn({ 'hidden ': !showInfo })}>
           <Info />
         </div>
       </main>
-      <Footer />
-    </>
+    </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
-import Head from "next/head";
-import { Poppins } from "next/font/google";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import CountDown from "@/sections/Home/CountDown";
-import Hero from "@/sections/Home/Hero";
-import HowToMint from "@/sections/Home/HowToMint";
-import AboutDrop from "@/sections/Home/AboutDrop";
-import RoadMap from "@/sections/Home/RoadMap";
-import OurTeam from "@/sections/Home/OurTeam";
-import { useState } from "react";
-import { testNft, nft } from "./api/testdb";
-import { Modal } from "@/components/Modal";
+import Head from 'next/head';
+import { Poppins } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import CountDown from '@/sections/Home/CountDown';
+import Hero from '@/sections/Home/Hero';
+import HowToMint from '@/sections/Home/HowToMint';
+import AboutDrop from '@/sections/Home/AboutDrop';
+import RoadMap from '@/sections/Home/RoadMap';
+import OurTeam from '@/sections/Home/OurTeam';
+import Layout from '@/components/Layout';
+import { useState } from 'react';
+import { testNft, nft } from './api/testdb';
+import { Modal } from '@/components/Modal';
 
 const poppins = Poppins({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
 });
 
 export default function Home() {
@@ -28,16 +29,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Modal {...nft} />
-      <main className={`${poppins.className} bg-white text-black`}>
-        <Navbar />
-        <Hero />
-        <CountDown duration={120122545} {...nft} />
-        <HowToMint />
-        <AboutDrop />
-        <RoadMap />
-        <OurTeam />
-        <Footer />
-      </main>
+
+      <Layout>
+        <main className={`${poppins.className} bg-white text-black`}>
+          <Hero />
+          <CountDown duration={120122545} {...nft} />
+          <HowToMint />
+          <AboutDrop />
+          <RoadMap />
+          <OurTeam />
+        </main>
+      </Layout>
     </>
   );
 }
