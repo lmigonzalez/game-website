a875a22671edf4f506193ef5622a5088e228b47f
joszze <jossecarlos@gmail.com>
2023-03-16 13:14:07 -0400

    nft Details refactored

--- src/components/NftDetail.tsx
+++ src/components/NftDetail.tsx
@@ -13,74 +13,74 @@ export const NftDetail: FC<nft> = (nft) => {
     return () => clearInterval(intervalId);
   }, []);
   return (
-    <div
-      className={` flex flex-col justify-between  md:flex-row md:items-center `}
-    >
-      <div className="flex justify-end  ">
-        <Image
-          className=" rounded-3xl"
-          src="/C783SC6k02TS2lt15FiMpEypcfXDFJ2kW4aGeDMy 2.svg"
-          alt="kds-tickets-m"
-          width={640}
-          height={270}
-        />
-      </div>
-      <div className="mx-6 flex flex-col md:gap-4  ">
-        <span className="text-[#292929] lg:text-2xl">NFT #{nft.id}</span>
-        <div className="flex flex-row gap-2 whitespace-nowrap">
-          <span className=" text-3xl font-bold lg:text-6xl">
-            Public Mint is
-          </span>
-          <div
-            className={`items-center justify-center gap-2 rounded-2xl text-3xl ${
+    <div className="card bg-base-100  md:card-side ">
+      <Image
+        className="rounded-3xl lg:h-max"
+        src="/C783SC6k02TS2lt15FiMpEypcfXDFJ2kW4aGeDMy 2.svg"
+        alt="Album"
+        width={622}
+        height={580}
+      />
+
+      <div className="card-body mt-2 ">
+        <strong className="text-[21px] font-semibold"> NFT #{nft.id}</strong>
+        <h2 className="card-title my-2 text-[40px] font-bold">
+          Public Mint is{" "}
+          <button
+            className={`rounded-xl bg-green-700 py-2 px-2 text-white ${
               nft.live ? `bg-green-700` : `bg-red-700`
-            }`}
+            })`}
           >
-            <span className=" m-5 font-bold text-white lg:text-6xl">
-              {nft.live ? `Live` : `Off`}
-            </span>
-          </div>
+            {nft.live ? `Live` : `Off`}
+          </button>
+        </h2>
+        <div className="mt-2 ">
+          Mint end in:{" "}
+          <strong className="font-semibold">
+            {timeUnits(timeLeft)?.days !== 0
+              ? `${timeUnits(timeLeft)?.days} hours: `
+              : " "}
+            {timeUnits(timeLeft)?.hours !== 0
+              ? `${timeUnits(timeLeft)?.hours} hours: `
+              : " "}
+            {timeUnits(timeLeft)?.minutes !== 0
+              ? `${timeUnits(timeLeft)?.minutes} hours: `
+              : " "}
+            {`${timeUnits(timeLeft)?.seconds} sec`}.
+          </strong>
+          <br />
+          <br />
+          Whitelist: <strong className=" font-semibold">{nft.whitelist}</strong>
+          <br />
+          Presale: <strong className=" font-semibold">
+            {nft.presale}
+          </strong>{" "}
+          <br />
+          <br />
+          <span className=" rounded-full border border-solid px-3">
+            {nft.maxCount} MAX PER WALLET
+          </span>
         </div>
-        <span className="lg:text-xl">
-          Mint end in:
-          {timeUnits(timeLeft)?.days !== 0
-            ? `${timeUnits(timeLeft)?.days} hours: `
-            : " "}
-          {timeUnits(timeLeft)?.hours !== 0
-            ? `${timeUnits(timeLeft)?.hours} hours: `
-            : " "}
-          {timeUnits(timeLeft)?.minutes !== 0
-            ? `${timeUnits(timeLeft)?.minutes} hours: `
-            : " "}
-          {`${timeUnits(timeLeft)?.seconds} sec`}
-        </span>
-        <div className="flex flex-col">
-          <span className=" text-xl">Whitelist: {nft.whitelist}</span>
-          <span className=" text-xl">Presale: {nft.presale}</span>
-        </div>
-        <div className="w-fit rounded-full border border-black text-center">
-          <span className="m-5 text-xl">{nft.maxCount} MAX PER WALLET</span>
-        </div>
-        <div className=" flex flex-row gap-28">
-          <div className="flex flex-col">
-            <span className="lg:text-xl"> Price:</span>
-            <span className="text-2xl font-bold lg:text-4xl">
-              {nft.price} ETH
-            </span>
-          </div>
-          <div className="flex flex-col">
-            <span className="lg:text-xl"> Remaining:</span>
-            <span className="text-2xl font-bold lg:text-4xl">
+        <div className=" mb-16 mt-4 flex flex-row">
+          <p>
+            Price: <br />
+            <strong className=" text-[28px]">{nft.price} ETH</strong>
+          </p>{" "}
+          <p>
+            Remaining: <br />
+            <strong className=" text-[28px]">
               {nft.packsLeft}/{nft.packsCount}
-            </span>
-          </div>
+            </strong>
+          </p>
         </div>
-        <label
-          htmlFor="my-modal"
-          className="orange-gradient btn h-full w-full rounded-full border-none text-xl normal-case"
-        >
-          Mint Now
-        </label>
+        <div className="card-actions ">
+          <label
+            htmlFor="my-modal"
+            className="orange-gradient btn-circle btn h-14 w-[478px] border-none text-base normal-case text-white"
+          >
+            Mint Now
+          </label>
+        </div>
       </div>
     </div>
   );
