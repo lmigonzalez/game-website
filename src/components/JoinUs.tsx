34e77250a256cbfd9bd3b1a8c3cf8fe2da406462
joszze <jossecarlos@gmail.com>
2023-03-18 11:57:12 -0400

    fix responsive JoinUs

--- src/components/JoinUs.tsx
+++ src/components/JoinUs.tsx
@@ -7,14 +7,14 @@ export const JoinUs = () => {
       whileInView={{ opacity: 1 }}
       viewport={{ once: true, amount: 0.8 }}
       transition={{ delay: 0.3 }}
-      className="orange-gradient m-auto mt-48 flex w-[1400px] max-w-full flex-col items-center justify-center rounded-lg py-16 text-white"
+      className="orange-gradient m-auto mt-48 flex w-[1400px] max-w-full flex-col items-center justify-center rounded-lg py-8 text-white"
     >
       <motion.h3
         initial={{ y: -50, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         viewport={{ once: true, amount: 0.8 }}
         transition={{ delay: 0.6 }}
-        className=" mb-8 text-6xl font-bold"
+        className=" mb-8 text-xl font-bold md:text-6xl"
       >
         Join Our Discord Group
       </motion.h3>
