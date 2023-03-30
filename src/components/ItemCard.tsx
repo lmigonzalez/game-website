08a66d0e5dc188f5a884dfa006ec22d65726a5af
joszze <jossecarlos@gmail.com>
2023-03-30 14:05:55 -0400

    fix item equip

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -6,26 +6,22 @@ const ItemCard: FC<{ img: ImageLoaderProps; equipped: boolean }> = ({
   equipped,
 }) => {
   return (
-    <div className="container flex h-fit w-fit flex-col items-center justify-center gap-3 lg:justify-start lg:gap-4">
+    <div className="flex h-fit w-fit flex-col items-center justify-center gap-3 whitespace-nowrap md:gap-4 lg:justify-start">
       <Image
         src={img.src}
         alt={"no img"}
         width={182.48}
         height={182.48}
-        className=" rounded-2xl lg:h-[337px] lg:w-[337px]"
+        className=" w-full rounded-2xl"
       />
       {!equipped ? (
-        <div className="orange-gradient flex h-[24.37px] w-[103.97px] items-center justify-center rounded-full p-[1px] pl-[1px] text-[8.66px] lg:h-[45px] lg:w-[192px] lg:text-base">
-          <button className="orange-text flex h-full w-full items-center justify-center rounded-full bg-[#dcdce7]">
-            <p className="orange-gradient bg-clip-text text-transparent">
-              Equip
-            </p>
-          </button>
-        </div>
+        <button className="flex h-[24.37px] w-[103.97px] items-center justify-center rounded-3xl border-2 border-[#DB0F29] py-2  md:h-[45px] md:w-[192px] md:text-base">
+          Equip
+        </button>
       ) : (
-        <button className="h-[24.37px] w-[103.97px] lg:h-[45px] lg:w-[192px]">
-          <div className="orange-gradient 4 flex h-full w-full items-center justify-center rounded-3xl text-[8.66px] text-[#FCDDEC]">
-            Equiped
+        <button className="h-[24.37px] w-[103.97px] md:h-[45px] md:w-[192px]">
+          <div className="orange-gradient flex h-full w-full items-center justify-center rounded-3xl text-[8.66px] text-[#FCDDEC] md:text-base">
+            Equipped
           </div>
         </button>
       )}
